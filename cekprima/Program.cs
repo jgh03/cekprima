ee0a60bac88c2dd4b62d825d39f1319a972c8229
jgh03 <jgabrielle@student.ciputra.ac.id>
2021-12-03 10:47:19 +0700

    Initial check-in of module cekprima

---
+++ cekprima/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace cekprima
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("input bilangan:");
+            int bilangan = Convert.ToInt32(Console.ReadLine());
+            if (bilangan > 0 && bilangan % 2 == 1)
+            {
+                Console.Write("bilangan prima");
+            }
+            else
+            {
+                Console.Write("bukan bilangan prima");
+            }
+        }
+    }
+}
